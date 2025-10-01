7e0e9212c71a2d8919793456c338ea972fe6fed7
Mickeydeveloper <mickdenzel24@gmail.com>
2025-10-01 09:39:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ import { getCreds } from './credits.js'
 
 const creds = await getCreds()
 
-export const TELEGRAM_BOT_TOKEN = ''; // bot telegram token
+export const TELEGRAM_BOT_TOKEN = '8338344513:AAHjfJGcPcVQH4ZBUNRqC0ZxIj96-0Gcclo'; // bot telegram token
 
 export const REDIRECT_BOT = "None" // a redirect bot when the bot is full
 
-export const OWNER_ID = "" // owner id
+export const OWNER_ID = "8050573929" // owner id
 
 export const LIMIT = 30; //number of max sessions for the tele bot
 
